4c13a419eabe2194c4e044fe3745e52781dea0f2
brian-christopher <brian-christopher1995@outlook.com>
2025-06-27 18:09:26 -0300

    Refactor `ToVector2` method to use `ToVector2I` and add `ToVector2I` method to `GodotExtensions`

--- src/Core/Extensions/GodotExtensions.cs
+++ src/Core/Extensions/GodotExtensions.cs
@@ -7,13 +7,18 @@ public static class GodotExtensions
 {
     public static Vector2 ToVector2(this Heading heading)
     {
+        return ToVector2I(heading);
+    }
+    
+    public static Vector2I ToVector2I(this Heading heading)
+    {
         return heading switch
         {
-            Heading.South => Vector2.Up,
-            Heading.North => Vector2.Down,
-            Heading.West => Vector2.Left,
-            Heading.East => Vector2.Right,
-            _ => Vector2.Zero,
+            Heading.South => Vector2I.Up,
+            Heading.North => Vector2I.Down,
+            Heading.West => Vector2I.Left,
+            Heading.East => Vector2I.Right,
+            _ => Vector2I.Zero,
         };
     }
 }
