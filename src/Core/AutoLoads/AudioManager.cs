95bd320893ac61c2a4d554e7e65320329abfa171
brian-christopher <brian-christopher1995@outlook.com>
2025-06-29 08:36:37 -0300

    Add `AudioManager` autoload for handling audio playback

---
+++ src/Core/AutoLoads/AudioManager.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using Godot;
+
+namespace ArgentumOnline.Core.AutoLoads;
+
+public partial class AudioManager : Node
+{
+    public static AudioManager Instance { get; private set; }
+    
+    public AudioManager()
+    {
+        Debug.Assert(Instance == null, "Only one instance of AudioManager is allowed.");
+        Instance = this;
+    }
+
+    public void PlayAudio(int waveId)
+    {
+        if (!ResourceLoader.Exists($"res://assets/sounds/{waveId}.wav"))
+        {
+            GD.PrintErr($"Audio file {waveId} not found.");
+            return;
+        }
+        
+        AudioStream audioStream = ResourceLoader.Load<AudioStream>($"res://assets/sounds/{waveId}.wav");
+        
+        AudioStreamPlayer streamPlayer = new();
+        streamPlayer.Bus = "sfx";
+        streamPlayer.Finished += () => streamPlayer.QueueFree();
+        streamPlayer.Play();
+        
+        AddChild(streamPlayer);
+    }
+}
