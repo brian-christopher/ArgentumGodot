bba4872951bb41facb998975878e113213ec81cf
brian-christopher <brian-christopher1995@outlook.com>
2025-06-30 07:37:15 -0300

    Add `Skill` enum

---
+++ src/Core/Types/Skill.cs
@@ -0,0 +1,29 @@
+namespace ArgentumOnline.Core.Types;
+
+public enum Skill
+{
+    None = 0,
+    Magia = 1,
+    Robar = 2,
+    Tacticas = 3,
+    Armas = 4,
+    Meditar = 5,
+    Apunalar = 6,
+    Ocultarse = 7,
+    Supervivencia = 8,
+    Talar = 9,
+    Comerciar = 10,
+    Defensa = 11,
+    Pesca = 12,
+    Mineria = 13,
+    Carpinteria = 14,
+    Herreria = 15,
+    Liderazgo = 16,
+    Domar = 17,
+    Proyectiles = 18,
+    Wrestling = 19,
+    Navegacion = 20,
+
+// FIXME: Revisar esto!
+    FundirMetal = 88
+}
