6a3b526a26b179172fc83f59dfca01651670134e
brian-christopher <brian-christopher1995@outlook.com>
2025-06-30 07:22:55 -0300

    Handle left-click, double-click, and work-click events in `GameUIController` with mouse position calculations and `NetworkClient` integration

--- src/Core/GameScreen.cs
+++ src/Core/GameScreen.cs
@@ -41,6 +41,7 @@ public partial class GameScreen : Node
         NetworkClient.Instance.OnReceive += InstanceOnOnReceive;
         
         NetworkClient.Instance.ConnectToHost("127.0.0.1", 7666);
+        UIController.GameContext = _gameContext;
     }
 
     public override void _Process(double delta)
--- src/UI/GameUIController.cs
+++ src/UI/GameUIController.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Threading.Tasks;
+using ArgentumOnline.Core;
+using ArgentumOnline.Core.AutoLoads;
+using ArgentumOnline.Core.Extensions;
 using ArgentumOnline.Data;
+using ArgentumOnline.Net;
 using Godot;
 
 namespace ArgentumOnline.UI;
@@ -10,6 +14,47 @@ public partial class GameUIController : CanvasLayer
     [Export] public Camera2D MainCamera { get; set; }
     [Export] public RichTextLabel ConsoleOutput { get; set; }
     
+    public GameContext GameContext { get; set; }
+
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton button)
+        {
+            HandleEventMouseButton(button);
+        }
+    }
+
+    private void HandleEventMouseButton(InputEventMouseButton mouseEvent)
+    {
+        Vector2 mouseViewPosition= MainCamera.GetCanvasTransform().AffineInverse() * mouseEvent.Position;
+        Vector2I mouseTilePosition = (mouseViewPosition / 32.0f).Ceil().ToVector2I();
+
+        if (GameContext.Trading)
+        {
+            return;
+        }
+
+        if (mouseEvent.Pressed && mouseEvent.ButtonIndex == MouseButton.Left)
+        {
+            if (mouseEvent.DoubleClick)
+            {
+                NetworkClient.Instance.SendDoubleClick(mouseTilePosition.X, mouseTilePosition.Y);
+                return;
+            }
+
+            if (GameContext.UsingSkill == 0)
+            {
+                NetworkClient.Instance.SendLeftClick(mouseTilePosition.X, mouseTilePosition.Y);
+            }
+            else
+            {
+                NetworkClient.Instance.SendWorkLeftClick(mouseTilePosition.X, mouseTilePosition.Y, GameContext.UsingSkill);
+                GameContext.UsingSkill = 0;
+            }
+        }
+    }
+
     public void WriteToConsole(string message, FontData font)
     {
         string bbcode = FormatBBCode(message, font);
